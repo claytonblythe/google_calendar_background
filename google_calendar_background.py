d617544f3f20d9e7811c0d0ba6895c164a548f82
Clayton Blythe <claytonb@zg03162.zillow.local>
2019-07-08 10:23:42 -0700

    change transparency

--- google_calendar_background.py
+++ google_calendar_background.py
@@ -14,7 +14,7 @@ from oauth2client import file
 
 
 def get_wallpaper_image(size=(3840, 2160)):
-    return Image.open("./resources/mojave_night.jpg").resize(size=size)
+    return Image.open("./resources/dark_starry_night.jpg").resize(size=size)
 
 
 def draw_events(img, events, fill=(230, 230, 230)):
@@ -30,7 +30,9 @@ def draw_events(img, events, fill=(230, 230, 230)):
         d.text((x_offset, y_offset + 80 * idx), time, fill=fill, font=font)
         d.text((description_offset, y_offset + 80 * idx), summary, fill=fill, font=font)
     zillow_price = round(float(get_stock_quote('Z')), 2)
+    vtsax_price = round(float(get_stock_quote('VTSAX')), 2)
     d.text((3000, 430), f"Z price: {zillow_price}", fill=fill, font=font)
+    d.text((3000, 510), f"VTSAX : {vtsax_price}", fill=fill, font=font)
     return img
 
 
@@ -45,7 +47,6 @@ def make_calendar(events):
     calendar = draw_events(img, events)
     weather_img = get_weather_img()
     img.paste(weather_img, (3000, 30), weather_img)
-    zillow_price = get_stock_quote('Z')
     change_desktops("./resources/black.jpg")
     calendar.save('calendar.jpg')
 
@@ -60,15 +61,18 @@ def change_desktops(default_file=None):
 
 
 def get_weather_img():
-    resp = requests.get(url="https://wttr.in/Seattle_tqp0_transparency=120.png")
+    resp = requests.get(url="https://wttr.in/Seattle_tqp0_transparency=220.png")
     return Image.open(BytesIO(resp.content)).resize((800, 400), resample=True)
 
 def get_stock_quote(ticker):
-    with open("./alphavantage_creds.json") as creds:
-        token = json.loads(creds.read())["token"]
-    url = f"https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol={ticker}&apikey={token}"
-    r = requests.get(url)
-    return r.json()['Global Quote']['05. price']
+    try:
+        with open("./alphavantage_creds.json") as creds:
+            token = json.loads(creds.read())["token"]
+        url = f"https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol={ticker}&apikey={token}"
+        r = requests.get(url)
+        return r.json()['Global Quote']['05. price']
+    except Exception as e:
+        print(e)
 
 def main():
     now = datetime.utcnow()
